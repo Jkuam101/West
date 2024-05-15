d9c96ae6de39db95ecd7719b7418d2386cd2d11a
Jkuam101 <kuamsalvador.com>
2024-05-14 20:36:03 -0500

    upadted vpc

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,14 @@ resource "aws_instance" "allstar" {
     Name = "test-spot"
   }
 }
+
+
+
+resource "aws_instance" "allstar" {
+  ami ="ami-0ddda618e961f2270"
+  instance_type = "t2.micro"
+  key_name = "West37"
+  tags = {
+    Name = "test-spot"
+  }
+}
